fa7fb7fba89b4564ad94168fc80834c0b71488d3
jessiesj <jessiesj9@163.com>
2025-09-24 16:17:37 +0800

    test github page

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  // 配置GitHub Pages的基础路径，已经根据实际仓库名称修改
-  // 仓库地址是 https://jessiesj.github.io/taro/
-  // 所以base设置为 '/taro/'
-  base: '/taro/',
+  // 使用hash路由模式，不需要特殊的base配置
+  base: '/',
 })
