97e38f9794f55df0aa235f830d30cc6c6e21ea44
jessiesj <jessiesj9@163.com>
2025-09-24 15:39:54 +0800

    try to fix no shown in pages

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  // 配置GitHub Pages的基础路径，需要根据实际仓库名称修改
+  // 如果仓库地址是 https://username.github.io/repo-name/
+  // 则base应该设置为 '/repo-name/'
+  base: '/', // 默认值，用户需要根据自己的仓库名称修改
 })
